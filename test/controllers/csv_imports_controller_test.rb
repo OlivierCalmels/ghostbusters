142be451f5547a9a4c041371353cab01180c9150
OlivierCalmels <calmels.o@gmail.com>
2021-01-30 00:30:48 +0100

    generate csv_import controller, First_table_controller useless and second_table_controller useless too.

--- test/controllers/csv_imports_controller_test.rb
+++ test/controllers/csv_imports_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class FirstTableRecordControllerTest < ActionDispatch::IntegrationTest
+class CsvImportsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
