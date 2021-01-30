ff22b63793e1c6857f50e6cef18b0f576e082cf9
OlivierCalmels <calmels.o@gmail.com>
2021-01-30 14:35:31 +0100

    Add csv files for app testing and date treatment

--- app/controllers/csv_imports_controller.rb
+++ app/controllers/csv_imports_controller.rb
@@ -9,19 +9,16 @@ class CsvImportsController < ApplicationController
     import_second_csv
   end
 
-  def import_first_csv(filepath = 'Namibia teachers data EMIS.csv')
+  def import_first_csv(filepath = 'data/EMIS_crop.csv', col_sep = ";")
     # filepath = 'Namibia teachers data EMIS.csv'
-    csv_option = { col_sep: ',', headers: true }
+    csv_option = { col_sep: ';', headers: true }
     CSV.foreach(filepath, csv_option) do |row|
       emis_number = row[0]
       teacher_surname = row[1]
       teacher_name = row[2]
       teacher_sex = row[3]
       dob = row[4]
-      # JJ/MM/AA en string
-      # migrateion date => string
-      # 15396 => 15/30/56
-      # raise
+      raise
       table_record = FirstTableRecord.create!(  emis_number: emis_number,
                                                 teacher_surname: teacher_surname,
                                                 teacher_name: teacher_name,
@@ -31,7 +28,7 @@ class CsvImportsController < ApplicationController
     end
   end
 
-  def import_second_csv(filepath = 'Namibia teachers data Payroll.csv')
+  def import_second_csv(filepath = 'data/Payroll_crop.csv')
     # filepath = 'Namibia teachers data Payroll.csv'
     csv_option = { col_sep: ',', headers: true }
     CSV.foreach(filepath, csv_option) do |row|
