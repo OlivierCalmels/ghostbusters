a3167746de44d15c723e8ac24c541cc9904b5146
OlivierCalmels <calmels.o@gmail.com>
2021-01-31 16:10:57 +0100

    Export to file works

--- app/models/csv_export.rb
+++ app/models/csv_export.rb
@@ -2,9 +2,9 @@ class CsvExport < ApplicationRecord
   require 'csv'
 
   def self.export(attributes = {}) # filename, file_directory, data (array to export, first row as headers) col_sep (optional)
-    data_default = [ %w(header1 header2 header3), %w(r1c1 r1c2 r1c3), %w(r2c1 r2c2 r2c3), %w(r3c1 r3c2 r3c3)]
-    filename = attributes[:filename] # || "export-#{Date.today}"
-    file_directory = attributes[:file_directory] # || "data/"
+    # data_default = [ %w(header1 header2 header3), %w(r1c1 r1c2 r1c3), %w(r2c1 r2c2 r2c3), %w(r3c1 r3c2 r3c3)]
+    filename = attributes[:filename]  || "Filtered_1:1_correlation.csv"
+    file_directory = attributes[:file_directory]  || "public/"
     col_sep = attributes[:col_sep] || ";"
     data = attributes[:data] # || data_default
 
